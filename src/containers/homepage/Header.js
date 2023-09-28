3988ce741385dc503ef47ea528fcc3e3451ae971
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-28 10:54:25 +0700

    edited margin and padding

--- src/containers/homepage/Header.js
+++ src/containers/homepage/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div className="flex justify-center border-t-2 border-b-2 border-gray-300">
+      <div className="flex justify-center border-t-2 border-b-2 border-gray-300 my-10">
         <h1 className="pb-5 text-[220px] leading-none text-gray-900 font-bold align-middle">
           THE BLOG
         </h1>
