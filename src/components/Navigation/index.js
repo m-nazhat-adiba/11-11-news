ab466a1a925c491032d0458456b509c0028554ab
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-28 10:53:35 +0700

    edited margin

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex w-full justify-between py-5">
+    <div className="flex w-full justify-between py-8">
       <Image src="/logo.svg" width={80} height={30} alt="logo" />
       <ul className="flex gap-4 text-gray-900">
         <li>Home</li>
