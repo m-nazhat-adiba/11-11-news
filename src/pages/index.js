44399033b68f54d74174ddc918ab039e8ecb147a
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-28 10:05:52 +0700

    added margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,30 @@
 import Layout from "@/components/Layout";
 import Header from "@/containers/homepage/Header";
+import Highlight from "@/containers/homepage/Highlight";
 import React from "react";
+import axios from "axios";
 
-const index = () => {
+const index = ({ data, users }) => {
   return (
     <Layout>
       <Header />
+      <div className="mx-40">
+        <Highlight data={data} users={users} />
+      </div>
     </Layout>
   );
+};
+
+export const getServerSideProps = async () => {
+  // posts endpoint
+  const res1 = await axios.get("https://dummyjson.com/posts");
+  const data = await res1.data.posts;
+
+  //users endpoint
+  const res2 = await axios.get("https://dummyjson.com/users?limit=0");
+  const users = await res2.data.users;
+
+  return { props: { data, users } };
 };
 
 export default index;
