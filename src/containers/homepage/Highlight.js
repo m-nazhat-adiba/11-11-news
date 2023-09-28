dab9f954a72c1d8154cefbde5cfdfbb4a28c9a79
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-28 10:06:26 +0700

    first commit

---
+++ src/containers/homepage/Highlight.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PostCard from "@/components/common/PostCard";
+
+const Highlight = ({ data, users }) => {
+  return (
+    <div>
+      <h1 className="text-gray-900 text-xl font-semibold my-6">
+        Recent blog posts
+      </h1>
+      <div className="flex">
+        {/* Main Content */}
+        <div className="flex flex-col gap-4">
+          {data.map((item, key) => (
+            <PostCard
+              data={item}
+              key={key}
+              user={users.filter((acc) => acc.id === item.userId)}
+              className="w-[600px]"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Highlight;
