9482bd1f33315a27ca80035e760f9ea424b20a68
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-29 10:02:21 +0700

    added Panel element

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,16 @@ import Highlight from "@/containers/homepage/Highlight";
 import React from "react";
 import axios from "axios";
 import SideContent from "@/containers/homepage/SideContent";
+import UserPanel from "@/containers/homepage/UserPanel";
 
 const index = ({ data, users }) => {
   return (
     <Layout>
       <Header />
-      <div className="flex justify-between mx-40 pb-20">
+      <div className="flex justify-between mx-28 pb-20">
+        <div className="relative">
+          <UserPanel />
+        </div>
         <Highlight data={data} users={users} />
         <div className="relative">
           <SideContent
@@ -24,7 +28,7 @@ const index = ({ data, users }) => {
 
 export const getServerSideProps = async () => {
   // posts endpoint
-  const res1 = await axios.get("https://dummyjson.com/posts");
+  const res1 = await axios.get("https://dummyjson.com/posts?limit=20");
   const data = await res1.data.posts;
 
   //users endpoint
